1ad8de70a73c9f1970c4076b81dda11ed774665e
ademrizki <ade.rizky@amartha.com>
2022-07-08 17:27:03 +0700

    Create sign in model

---
+++ lib/models/sign_in_model.dart
@@ -0,0 +1,15 @@
+class SignInModel {
+  String? token;
+
+  SignInModel({this.token});
+
+  SignInModel.fromJson(Map<String, dynamic> json) {
+    token = json['token'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['token'] = token;
+    return data;
+  }
+}

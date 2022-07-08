d21438ef3d9a4110f99ae6da2b55743e8ee7e015
ademrizki <ade.rizky@amartha.com>
2022-07-08 17:26:14 +0700

    Add finite state and catch block

--- lib/provider/user_provider.dart
+++ lib/provider/user_provider.dart
@@ -1,15 +1,33 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:test_api_app/models/users_model.dart';
 import 'package:test_api_app/services/services.dart';
+import 'package:test_api_app/utils/state/finite_state.dart';
 
 class UserProvider extends ChangeNotifier {
   final ApiService service = ApiService();
 
   UsersModel? users;
 
-  Future fetchUsersData() async {
-    users = await service.fetchUsers();
+  MyState myState = MyState.loading;
 
-    notifyListeners();
+  Future fetchUsersData() async {
+    try {
+      myState = MyState.loading;
+      notifyListeners();
+
+      users = await service.fetchUsers();
+
+      myState = MyState.loaded;
+      notifyListeners();
+    } catch (e) {
+      if (e is DioError) {
+        /// If want to check status code from service error
+        e.response!.statusCode;
+      }
+
+      myState = MyState.failed;
+      notifyListeners();
+    }
   }
 }

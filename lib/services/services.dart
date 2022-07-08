f1fecaaf028d70b3e89091d5760d5d5f0daccd55
ademrizki <ade.rizky@amartha.com>
2022-07-08 17:25:30 +0700

    Add login service

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,9 +1,30 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
+import 'package:test_api_app/models/sign_in_model.dart';
 import 'package:test_api_app/models/user_detail_model.dart';
 import 'package:test_api_app/models/users_model.dart';
 
 class ApiService {
+  ApiService() {
+    _dio.interceptors.add(InterceptorsWrapper(onRequest: (options, handler) {
+      // Do something before request is sent
+      return handler.next(options); //continue
+      // If you want to resolve the request with some custom data，
+      // you can resolve a `Response` object eg: `handler.resolve(response)`.
+      // If you want to reject the request with a error message,
+      // you can reject a `DioError` object eg: `handler.reject(dioError)`
+    }, onResponse: (response, handler) {
+      // Do something with response data
+      return handler.next(response); // continue
+      // If you want to reject the request with a error message,
+      // you can reject a `DioError` object eg: `handler.reject(dioError)`
+    }, onError: (DioError e, handler) {
+      if (e.response!.statusCode == 401) {
+      } else {}
+      return handler.next(e); //continue
+      // If you want to resolve the request with some custom data，
+      // you can resolve a `Response` object eg: `handler.resolve(response)`.
+    }));
+  }
   final Dio _dio = Dio();
 
   final String _baseUrl = 'https://reqres.in/api/';
@@ -13,8 +34,7 @@ class ApiService {
       final response = await _dio.get(_baseUrl + 'users');
 
       return UsersModel.fromJson(response.data);
-    } catch (e) {
-      debugPrint(e.toString());
+    } on DioError catch (e) {
       rethrow;
     }
   }
@@ -25,7 +45,25 @@ class ApiService {
 
       return UserDetailModel.fromJson(response.data);
     } catch (e) {
-      debugPrint(e.toString());
+      rethrow;
+    }
+  }
+
+  Future<SignInModel> signIn({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await _dio.post(
+        _baseUrl + 'login',
+        data: {
+          'email': email,
+          'password': password,
+        },
+      );
+
+      return SignInModel.fromJson(response.data);
+    } on DioError catch (e) {
       rethrow;
     }
   }

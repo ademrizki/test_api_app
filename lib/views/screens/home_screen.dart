d3ef0e96fafebe01a3e7108042820da537928928
ademrizki <ade.rizky@amartha.com>
2022-10-26 21:00:36 +0700

    feature_insert_data: Add new feature

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -32,7 +32,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('List of User'),
+        title: const Text('Life is Worthless'),
       ),
       body: Consumer<UserProvider>(
         builder: (context, provider, _) {
